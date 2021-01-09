f0b3dcd9c8ab13e5ebdb42a46586acf284446ce0
caglar7 <mcaglarcnkr@gmail.com>
2021-01-09 19:11:08 +0300

    some testing

--- app/src/main/java/com/example/fragmentexample/MainActivity.java
+++ app/src/main/java/com/example/fragmentexample/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.fragmentexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Fragment1 fragment1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // set fragment
+        fragment1 = new Fragment1();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment1);
+        fragmentTransaction.commit();
     }
 }
